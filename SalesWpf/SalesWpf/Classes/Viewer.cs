1b0d3921efd0b10708ac50ab5e323931571cccee
Бойчев Александр <bojc_tq19@od.itstep.local>
2021-08-04 15:36:32 +0300

    add first hw

---
+++ SalesWpf/SalesWpf/Classes/Viewer.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace SalesWpf.Classes
+{
+    class Viewer
+    {
+        public String ConectString { get; set; }
+
+        public Viewer(string conectString)
+        {
+            ConectString = conectString;
+        }
+
+        public void SetNameTables(ComboBox list)
+        {
+            try
+            {
+                using (var connection = new SqlConnection(ConectString))
+                {
+                    connection.Open();
+                    DataTable schema = connection.GetSchema("Tables");
+                    for (int i = 0; i < schema.Rows.Count; i++)
+                    {
+                        ComboBoxItem newConmboElem = new ComboBoxItem();
+                        newConmboElem.Content = schema.Rows[i][2].ToString();
+                        list.Items.Add(newConmboElem);
+                    }
+                }
+            }
+            catch (DbException ex)
+            {
+                Console.WriteLine($"Error with work BD: {ex.Message}");
+            }
+        }
+
+        public void LoadNewDataTable(string Question, ListBox listData)
+        {
+            SendQuest(Question, listData);
+        }
+
+        private void SendQuest(string Question, ListBox listData)
+        {
+            try
+            {
+                using (var connection = new SqlConnection(ConectString))
+                {
+                    connection.Open();
+                    using (var command = new SqlCommand(Question, connection))
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        int countCol = reader.FieldCount;
+                        String title = "";
+                        for (int i = 1; i < countCol; i++)
+                        {
+                            title += $"{reader.GetName(i)}\t";
+                            if (i == countCol - 2 && countCol != 3)
+                            {
+                                title += '\t';
+                            }
+                        }
+                        // title += '\t';
+                        ListBoxItem newListBoxItem = new ListBoxItem();
+                        newListBoxItem.Content = title;
+                        listData.Items.Add(newListBoxItem);
+                        while (reader.Read())
+                        {
+                            ListBoxItem newElemBoxItem = new ListBoxItem(); ;
+                            for (int i = 1; i < countCol; i++)
+                            {
+                                newElemBoxItem.Content += ($"{reader.GetValue(i)}\t\t");
+                            }
+                            listData.Items.Add(newElemBoxItem);
+                        }
+                    }
+                }
+            }
+            catch (DbException ex)
+            {
+                Console.WriteLine($"Error with work BD: {ex.Message}");
+            }
+        }
+        public void ClearListNameTable(ListBox list)
+        {
+            list.Items.Clear();
+        }
+    }
+
+}
---
+++ SalesWpf/SalesWpf/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using SalesWpf.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SalesWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private Viewer ViewerObj { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string connectionString =
+           "Data Source=(local);Initial Catalog=Sales;" +
+           "Integrated Security=SSPI;Pooling=False";
+            ViewerObj = new Viewer(connectionString);
+            ViewerObj.SetNameTables(listNameTable);
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (listNameTable.Items.Count == 0)
+            {
+                return;
+            }
+            string nameTable = "";
+            ComboBoxItem selecItem = (ComboBoxItem)listNameTable.Items[listNameTable.SelectedIndex];
+            nameTable = (string)selecItem.Content;
+            ViewerObj.ClearListNameTable(ContentChoiceTable);
+            ViewerObj.LoadNewDataTable("SELECT* FROM " + nameTable, ContentChoiceTable);
+        }
+    }
+
+   
+}
